db702fe12124e1c8066e9b0b5a74541563508c53
Jordan Furr <you@example.com>
2020-04-21 17:02:33 -0400

    tab bar controller and hiding back button

--- FindFriends/SignUpViewController.swift
+++ FindFriends/SignUpViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 import FirebaseAuth
 
 class SignUpViewController: UIViewController, UITextFieldDelegate {
---
+++ FindFriends/TabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarController.swift
+//  FindFriends
+//
+//  Created by Jordan Furr on 4/21/20.
+//  Copyright © 2020 Jordan Furr. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
