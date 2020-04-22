60d240047d195f9115be473f3623242ceaa1ec18
Aaron Prestidge <aprestidge@gmail.com>
2020-04-22 11:54:29 -0600

    Add file structure, fix alignment of code in all files.

---
+++ FindFriends/Controllers/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,72 @@
+//
+//  SignUpViewController.swift
+//  FindFriends
+//
+//  Created by Jordan Furr on 4/21/20.
+//  Copyright © 2020 Jordan Furr. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var passwordConfirm: UITextField!
+    
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        if password.text != passwordConfirm.text {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else{
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "signupToHome", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var tF: [UITextField] = []
+        tF = [email, password, passwordConfirm]
+        for textfield in tF {
+            textfield.delegate = self
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
